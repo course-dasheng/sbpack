af17619a12ee69587a7fa96ad9fe9a38bc0b90c2
花果山大圣 <316783812@qq.com>
2022-06-02 22:41:11 +0800

    add javascript template

--- packages/sdk/src/templates-config.ts
+++ packages/sdk/src/templates-config.ts
@@ -1,4 +1,10 @@
 export const templatesConfig = {
+  "javascript": {
+    "index.html": "<div id=\"app\"></div>",
+    "index.js": "// Import stylesheets\nimport './style.css';\n\n// Write Javascript code!\nconst appDiv = document.getElementById('app');\nappDiv.innerHTML = `<h1>JS Starter</h1>`;",
+    "package.json": "{\n  \"name\": \"js\",\n  \"version\": \"0.0.0\",\n  \"private\": true,\n  \"dependencies\": {}\n}",
+    "style.css": "h1, h2 {\n  font-family: Lato;\n}"
+  },
   "nextjs": {
     ".babelrc": "{\n  \"presets\": [\"next/babel\"]\n}\n",
     "README.md": "This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).\n\n## Getting Started\n\nFirst, run the development server:\n\n```bash\nnpm run dev\n# or\nyarn dev\n```\n\nOpen [http://localhost:3000](http://localhost:3000) with your browser to see the result.\n\nYou can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.\n\n[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.\n\nThe `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.\n\n## Learn More\n\nTo learn more about Next.js, take a look at the following resources:\n\n- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.\n- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.\n\nYou can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!\n\n## Deploy on Vercel\n\nThe easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.\n\nCheck out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.\n",
